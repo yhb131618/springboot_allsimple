00340205ea5c2d5d14bb392c7c2e964ec1504605
yhb131618 <119278327+yhb131618@users.noreply.github.com>
2024-04-04 14:22:34 +0900

    '동영상_업로드_웹페이지'

--- FrontEnd/src/pages/VideoUploadPage/index.jsx
+++ FrontEnd/src/pages/VideoUploadPage/index.jsx
@@ -1,21 +1,34 @@
+import { TextField } from '@mui/material';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import axios from 'axios';
 import React, { useState } from 'react';
-
-
-
 
 const VideoUpload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
   // Assuming fixed values for bucketName and directoryName for simplicity
   // You can also set these from the UI as needed
-  const [bucketName, setBucketName] = useState('');
-  const [directoryName, setDirectoryName] = useState('');
+  const [bucketName, setBucketName] = useState('test');
+  const [directoryName, setDirectoryName] = useState('video');
   const [videoName, setVideoName] = useState(''); 
   const [videoTag, setVideoTag] = useState(''); 
-  const [videoLike, setVideoLike] = useState(''); 
-  const [videoDislike, setVideoDislike] = useState(''); 
+  const [videoBucketName, setVideoBucketName] = useState('test'); 
+  const [videoDirectoryName, setVideoDirectoryName] = useState('videoinfo'); 
+  const [thumbNailImage, setThumbNailImage] = useState(''); 
+  const [selectedThumbNailFile, setSelectedThumbNailFile] = useState(null);
+  const [thumbNailBucketName, seTthumbNailBucketName] = useState('test'); 
+  const [thumbNailDirectoryName, setThumbNailDirectoryName] = useState('thumbNail'); 
+  const [videoFileName, setVideoFileName] = useState(''); // 동영상 파일 이름을 저장할 상태
 
-
+  // 상태를 초기화하는 함수
+  const handleReset = () => {
+    setSelectedFile(null);
+    setVideoName('');
+    setVideoTag('');
+    setThumbNailImage('');
+    setVideoFileName('');
+  };
+ 
 
   // 비디오 이름과 시간을 업데이트하기 위한 핸들러 추가
   const handleVideoNameChange = (event) => {
@@ -25,109 +38,181 @@ const VideoUpload = () => {
   const handleVideoTagChange = (event) => {
     setVideoTag(event.target.value);
   };
-  const handleVideoLikeChange = (event) => {
-    setVideoLike(event.target.value);
-  };
 
-  const handleVideoDislikeChange = (event) => {
-    setVideoDislike(event.target.value);
-  };
   const handleFileChange = (event) => {
-    setSelectedFile(event.target.files[0]);
-  };
-  const handleBucketNameChange = (event) => {
-    setBucketName(event.target.value);
+    const file = event.target.files[0];
+    if (file) {
+      setSelectedFile(file); // 파일 상태 업데이트
+      setVideoFileName(`${file.name}:${bucketName}`);
+    }
+};
+
+  // 썸네일 파일 변경을 처리하는 핸들러
+  const handleThumbNailFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedThumbNailFile(file); // 선택된 썸네일 파일 상태 업데이트
+    previewFile(file); // 파일 미리보기 생성
   };
 
-  const handleDirectoryNameChange = (event) => {
-    setDirectoryName(event.target.value);
+  // 파일 미리보기를 생성하는 함수
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+        setThumbNailImage(reader.result); // 미리보기 URL을 상태로 설정
+    };
   };
-
 
   const handleUpload = async () => {
-    const formData = new FormData();
-    formData.append('file', selectedFile);
-    // Add bucketName and directoryName to the formData
-    formData.append('bucketName', bucketName);
-    formData.append('directoryName', directoryName);
+    // 파일 업로드를 위한 formData 준비
+    const uploadFormData = new FormData();
+    uploadFormData.append('file', selectedFile);
+    uploadFormData.append('bucketName', bucketName);
+    uploadFormData.append('directoryName', directoryName);
+    uploadFormData.append('videoFileName', videoFileName);
 
-      // Logging the formData keys and values
-    for (let key of formData.keys()) {
-        console.log(key, formData.get(key));
-    }
-    
-    // Logging the state values
-    console.log('Uploading with bucketName:', bucketName, 'and directoryName:', directoryName);
+    const thumbNailFormData = new FormData();
+    thumbNailFormData.append('file', selectedThumbNailFile);
+    thumbNailFormData.append('thumbNailBucketName', thumbNailBucketName);
+    thumbNailFormData.append('thumbNailDirectoryName', thumbNailDirectoryName);
+    thumbNailFormData.append('videoFileName', videoFileName);
 
-
-
+    // 비디오 정보 제출을 위한 formData 준비
+    const videoInfoFormData = new FormData();
+    videoInfoFormData.append('videoName', videoName);
+    videoInfoFormData.append('videoTag', videoTag);
+    videoInfoFormData.append('bucketName', videoBucketName);
+    videoInfoFormData.append('directoryName', videoDirectoryName);
+    videoInfoFormData.append('videoFileName', videoFileName);
+    // 필요한 경우 파일도 포함시킬 수 있습니다.
+    // videoInfoFormData.append('file', selectedFile);
+  
     try {
-      const response = await axios.post('http://localhost:8080/files/upload', formData, {
+      // 썸네일 업로드
+        const thumbNailResponsePromise = axios.post('http://localhost:8080/files/thumbNail', thumbNailFormData, {
+            headers: {
+            'Content-Type': 'multipart/form-data',
+            },
+        });
+      // 파일 업로드 요청
+      const uploadResponsePromise = axios.post('http://localhost:8080/files/upload', uploadFormData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      alert(JSON.stringify(response.data));
-      console.log('Upload response:', response.data);
-    } catch (error) {
-        alert(`Error uploading file: ${error.response.data.error}`);
-      console.error('Error uploading file:', error);
-      alert(`Error uploading file: ${error.response.data.error}`);
-    }
-  };
-
-  const handleSubmitVideoInfo = async () => {
-    const formData = new FormData();
-    formData.append('videoName', videoName);
-    formData.append('videoTag', videoTag);
-    formData.append('file', selectedFile); // 선택적으로 파일도 포함시킬 수 있습니다.
-    formData.append('videoLike', videoLike);
-    formData.append('videoDislike', videoDislike);
-    try {
-      const response = await axios.post('http://localhost:8080/files/video', formData, {
+  
+      // 비디오 정보 제출 요청
+      const videoInfoResponsePromise = axios.post('http://localhost:8080/files/video', videoInfoFormData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
       });
-      alert('Video info submitted successfully');
-      console.log(response.data);
+  
+      // 두 요청이 성공적으로 완료되었는지 확인
+      const [uploadResponse, videoInfoResponse, thumbNailResponse] = await Promise.all([uploadResponsePromise, videoInfoResponsePromise, thumbNailResponsePromise]);
+  
+      alert('File and video info submitted successfully');
+      console.log('Upload response:', uploadResponse.data);
+      console.log('Video info response:', videoInfoResponse.data);
+      console.log('thumbNailResponsePromise :', thumbNailResponse.data);
     } catch (error) {
-      console.error('Error submitting video info:', error);
-      alert('Error submitting video info');
+      console.error('Error uploading file or submitting video info:', error);
+      alert('Error uploading file or submitting video info');
     }
   };
+  
 
 
   
   return (
+
     <div className="w-full max-w-xs mx-auto space-y-4">
-      <input type="file" onChange={handleFileChange} 
-          className="block w-full text-sm text-gray-500
-          file:mr-4 file:py-2 file:px-4
-          file:rounded-full file:border-0
-          file:text-sm file:font-semibold
-          file:bg-violet-50 file:text-violet-700
-          hover:file:bg-violet-100"
-          />
-      {/* Update these inputs to use the new handlers */}
-      <input type="text"  onChange={handleBucketNameChange} placeholder="Bucket Name" />
-      <input type="text"  onChange={handleDirectoryNameChange} placeholder="Directory Name" />
+    <Typography component='h1' variant='h6'> 동영상 업로드 </Typography>
+    <div className="flex items-center space-x-2">
+    <input type="file" onChange={handleFileChange} 
+        className="block text-sm text-gray-500
+        file:mr-4 file:py-2 file:px-4
+        file:rounded-full file:border-0
+        file:text-sm file:font-semibold
+        file:bg-violet-50 file:text-violet-700
+        hover:file:bg-violet-100"
+        />
+    </div>
+      {/* 선택된 파일 이름을 화면에 표시 */}
+      {videoFileName && <Typography variant="subtitle1">선택된 동영상: {videoFileName}</Typography>}
+  
 
-      <button 
-      onClick={handleUpload}
-      className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-700"
-      >Upload</button>
+
+    <Typography component='h1' variant='h6'> 썸네일 업로드 </Typography>
+    <div className="flex items-center space-x-2">
+    <div>
+    <input type="file" accept="image/*" onChange={handleThumbNailFileChange} 
+        className="block text-sm text-gray-500
+        file:mr-4 file:py-2 file:px-4
+        file:rounded-full file:border-0
+        file:text-sm file:font-semibold
+        file:bg-violet-50 file:text-violet-700
+        hover:file:bg-violet-100"
+        />
+    {thumbNailImage && <img src={thumbNailImage} alt="Preview" style={{width: "100%", height: "auto"}} />}
+    </div>
+
+    </div>
+
+
+  
+ 
+        <Box
+            component="form"
+            sx={{
+                '& .MuiTextField-root': { m: 1, width: '25ch' },
+            }}
+            noValidate
+            autoComplete="off"
+            >
+            <Typography component='h' variant='h6'> ※ 동영상 제목을 입력해주세요 </Typography>
+            <div>
+                <TextField
+                required
+                id="outlined-required"
+                label="동영상 제목(추후 정규식 업데이트)"
+                placeholder="Video Name"
+                onChange={handleVideoNameChange}
+                variant="outlined"
+                />
+            </div>
+            <Typography component='h' variant='h6'> ※ 동영상 태그를 입력해주세요.</Typography>
+            <div>
+                <TextField
+                required
+                id="outlined-required"
+                label="하나만 입력해주세요(추후 여러 태그 업데이트)"
+                placeholder="Video Tag"
+                onChange={handleVideoTagChange}
+                variant="outlined"
+                />
+            </div>
+        </Box>
       
-      <p비디오 정보를 입력해주세요/>
-      <input type="text" onChange={handleVideoNameChange} placeholder="Video Name" />
-      <input type="text" onChange={handleVideoTagChange} placeholder="Video Tag" />
-      <input type="text" onChange={handleVideoLikeChange} placeholder="Video Like" />
-      <input type="text" onChange={handleVideoDislikeChange} placeholder="Video Dislike" />
+      {/* Update these inputs to use the new handlers */}
+    
 
-      <button 
-      onClick={handleSubmitVideoInfo} 
-      className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-700"
-      >Video Info</button>
+    <br>
+    </br>
+    <div className="flex  items-center space-x-1">
+        <button 
+          onClick={handleUpload}
+          className="px-4 py-2 bg-blue-500 text-white rounded-lg shadow hover:bg-blue-700"
+        >Upload</button>
+
+        <button 
+          onClick={handleReset}
+          className="px-4 py-2 bg-green-500 text-white rounded-lg shadow hover:bg-gray-700"
+        >Reset</button>
+      </div>
+
+      
+
     </div>
   );
 }
